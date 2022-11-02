02ada5b4686ad0e85a641c58bd6c5a883042656f
ursgarsan <ugsugs333@gmail.com>
2022-11-02 10:58:38 +0100

    .

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,13 +3,18 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+    @Query("SELECT * FROM products")
     List<Product> findAll();
+
+    @Query("SELECT * FROM products_types")
     List<ProductType> findAllProductTypes();
+
     Optional<Product> findById(int id);
     Product findByName(String name);
     Product save(Product p);
